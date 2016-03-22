bb6b8b80183aad441767aa89184bf2514a570c50
Siraj <sirajravel@gmail.com>
2016-03-22 16:00:15 -0700

    Update create_service.js

--- create_service.js
+++ create_service.js
@@ -1,5 +1,5 @@
-require('dotenv').load();
 var post = require('request').post;
+var env = require('./config.json');
 
 // Twilio User Notifications Service Endpoint
 var servicesUrl = 'https://notifications.twilio.com/v1/Services';
@@ -7,12 +7,12 @@ var servicesUrl = 'https://notifications.twilio.com/v1/Services';
 // Create a notifications service instance
 request.post(servicesUrl, {
   auth: {
-    username: process.env.TWILIO_ACCOUNT_SID,
-    password: process.env.TWILIO_AUTH_TOKEN
+    username: env.TWILIO_ACCOUNT_SID,
+    password: env.TWILIO_AUTH_TOKEN
   },
   form: {
     FriendlyName: 'My First Notifications App', 
-    ApnCredentialSid: process.env.TWILIO_CREDENTIAL_SID
+    ApnCredentialSid: env.TWILIO_CREDENTIAL_SID
   }
 }, function(err, httpResponse, body) {
   console.log(JSON.parse(httpResponse.body).sid);
