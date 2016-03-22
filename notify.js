7069ac2cddbfc5ccd4ea3e326e2ca0b546b2a727
Siraj <sirajravel@gmail.com>
2016-03-22 16:02:48 -0700

    Update notify.js

--- notify.js
+++ notify.js
@@ -1,18 +1,18 @@
-require('dotenv').load();
 var post = require('request').post;
+var env = require('./config.json');
 
 // Twilio User Notifications Service Endpoint
 var serviceUrl = 'https://notifications.twilio.com';
 
 // Notifications creation endpoint
 var notificationUrl = serviceUrl + '/v1/Services/'
-  + process.env.TWILIO_NOTIFICATION_SERVICE_SID + '/Notifications';
+  + env.TWILIO_NOTIFICATION_SERVICE_SID + '/Notifications';
 
 // Send a notification 
 post(notificationUrl, {
   auth: {
-    username: process.env.TWILIO_ACCOUNT_SID,
-    password: process.env.TWILIO_AUTH_TOKEN
+    username: env.TWILIO_ACCOUNT_SID,
+    password: env.TWILIO_AUTH_TOKEN
   },
   form: { 
     Identity: '' + process.argv.slice(2), 
