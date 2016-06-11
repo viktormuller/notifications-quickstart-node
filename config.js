fcea9f627e6ea0f3802f9937fd7fb86aca17c4bf
Eddie Zaneski <eddiezane@gmail.com>
2016-06-11 10:40:54 -0400

    Remove placeholder values from README.md

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "TWILIO_ACCOUNT_SID": "a", // Twilio Account SID from https://www.twilio.com/console
-  "TWILIO_AUTH_TOKEN": "a", // Twilio Auth Token from https://www.twilio.com/console
-  "TWILIO_APN_CREDENTIAL_SID": "a", // See https://www.twilio.com/docs/api/notifications/guides/configuring-ios-push-notifications
-  "TWILIO_GCM_CREDENTIAL_SID": "a", // See https://www.twilio.com/docs/api/notifications/guides/configuring-android-push-notifications
-  "TWILIO_NOTIFICATION_SERVICE_SID": "a" // Service SID from https://www.twilio.com/console/notify/services
+  "TWILIO_ACCOUNT_SID": "", // Twilio Account SID from https://www.twilio.com/console
+  "TWILIO_AUTH_TOKEN": "", // Twilio Auth Token from https://www.twilio.com/console
+  "TWILIO_APN_CREDENTIAL_SID": "", // See https://www.twilio.com/docs/api/notifications/guides/configuring-ios-push-notifications
+  "TWILIO_GCM_CREDENTIAL_SID": "", // See https://www.twilio.com/docs/api/notifications/guides/configuring-android-push-notifications
+  "TWILIO_NOTIFICATION_SERVICE_SID": "" // Service SID from https://www.twilio.com/console/notify/services
 }
