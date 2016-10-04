ebe876810796a569ce8f50f8b70728c0657c6aef
Viktor Muller <vmuller@twilio.com>
2016-10-04 11:41:32 +0100

    Added config info to config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "TWILIO_ACCOUNT_SID": "", // Twilio Account SID from https://www.twilio.com/console
-  "TWILIO_AUTH_TOKEN": "", // Twilio Auth Token from https://www.twilio.com/console
+  "TWILIO_ACCOUNT_SID": "ACddd55023e95e245f47506b888118f8f8", // Twilio Account SID from https://www.twilio.com/console
+  "TWILIO_AUTH_TOKEN": "32cb7c1a5f32ea043c604c69d908cfaa", // Twilio Auth Token from https://www.twilio.com/console
   "TWILIO_APN_CREDENTIAL_SID": "", // See https://www.twilio.com/docs/api/notifications/guides/configuring-ios-push-notifications
   "TWILIO_GCM_CREDENTIAL_SID": "", // See https://www.twilio.com/docs/api/notifications/guides/configuring-android-push-notifications
-  "TWILIO_NOTIFICATION_SERVICE_SID": "" // Service SID from https://www.twilio.com/console/notify/services
+  "TWILIO_NOTIFICATION_SERVICE_SID": "IS1e928b239609199df31d461071fd3d23" // Service SID from https://www.twilio.com/console/notify/services
 }
