556d41fa3be49917c3444a3136dfa862b4b2d793
Viktor Muller <vmuller@twilio.com>
2016-10-19 11:12:53 +0100

    Fix /register failing to generate response

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var env = require('./config.js');
 var twilio = require('twilio');
-var FB = require('fb');
+
+// Authenticate with Twilio
+var client = new twilio(env.TWILIO_ACCOUNT_SID,  env.TWILIO_AUTH_TOKEN);
+
+// Get a reference to the user notification service instance
+var service = client.notify.v1.services(env.TWILIO_NOTIFICATION_SERVICE_SID);
 
 // Create Express Webapp
 var app = express();
@@ -24,18 +29,14 @@ app.get('/', function(request, response) {
   });
 });
 
-function createBinding(identity, endpoint, bindingType, address){
-  // Authenticate with Twilio
-  var client = new twilio(env.TWILIO_ACCOUNT_SID,  env.TWILIO_AUTH_TOKEN);
-
-  // Get a reference to the user notification service instance
-  var service = client.notify.v1.services(env.TWILIO_NOTIFICATION_SERVICE_SID);
+//Create a binding using device properties
+app.post('/register', function(request, response) {
 
   service.bindings.create({
-    "endpoint": endpoint,
-    "identity": identity,
-    "bindingType": bindingType,
-    "address": address
+    "endpoint": request.body.endpoint,
+    "identity":  request.body.identity,
+    "bindingType": request.body.BindingType,
+    "address": request.body.Address
   }).then(function(binding) {
     var message = 'Binding created!';
     console.log(binding);
@@ -53,27 +54,30 @@ function createBinding(identity, endpoint, bindingType, address){
       message: message
     });
   });
-}
-
-//Create a binding using device properties
-app.post('/register', function(request, response) {
-  createBinding(request.body.identity, request.body.endpoint, request.body.BindingType, request.body.Address);
 });
 
 //Create a facebook-messenger binding based on the authentication webhook from Facebook
 app.post('/messenger_auth', function(request, response) {
+
+  response.status(200).send();
   //Extract the request received from Facebook
   var message = request.body.entry[0].messaging[0];
 
   console.log(message);
 
-  //Let's find out the first name of the user so that we can notify him/her using that later
-  fb = new FB.Facebook({});
-  FB.api('/' + message.sender.id, {access_token: env.FACEBOOK_PAGE_ACCESS_TOKEN}, function(resp){
-    var identity = resp.first_name;
-    var endpoint = 'FBM@' + identity;
-    //Let's create a new facebook-messenger Binding for our user
-    createBinding(identity, endpoint, 'facebook-messenger', message.sender.id);
+  var identity = message.sender.id;
+  var endpoint = 'FBM@' + identity;
+  //Let's create a new facebook-messenger Binding for our user
+  service.bindings.create({
+    "endpoint": endpoint,
+    "identity": identity,
+    "bindingType": 'facebook-messenger',
+    "address": identity
+  }).then(function(binding) {
+    console.log(binding);
+  }).catch(function(error) {
+    var message = 'Failed to create binding: ' + error;
+    console.log(message);
   });
 });
 
