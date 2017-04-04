aff5bfc54c4383f62022f4af12787999b17af59f
Kat King <kking@twilio.com>
2017-04-04 15:10:38 -0500

    Remove TWILIO_GCM_CREDENTIALS from notify quickstart.

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ module.exports = {
   "TWILIO_ACCOUNT_SID": "", // Twilio Account SID from https://www.twilio.com/console
   "TWILIO_AUTH_TOKEN": "", // Twilio Auth Token from https://www.twilio.com/console
   "TWILIO_APN_CREDENTIAL_SID": "", // See https://www.twilio.com/docs/api/notifications/guides/configuring-ios-push-notifications
-  "TWILIO_GCM_CREDENTIAL_SID": "", // See https://www.twilio.com/docs/api/notifications/guides/configuring-android-push-notifications
   "TWILIO_NOTIFICATION_SERVICE_SID": "", // Service SID from https://www.twilio.com/console/notify/services
   "FACEBOOK_PAGE_ACCESS_TOKEN": "" //Facebook page access token from https://developers.facebook.com/apps
 }
