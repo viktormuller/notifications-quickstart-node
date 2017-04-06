0193f8527164ff1084f98f6515c488421bd37042
Kat King <kking@twilio.com>
2017-04-06 15:25:19 -0400

    Remove TWILIO_APN_CREDENTIAL_SID from starter app.

--- create_service.js
+++ create_service.js
@@ -9,13 +9,6 @@ var serviceData = {
   friendlyName: 'My First Notifications App'
 }
 
-if (env.TWILIO_APN_CREDENTIAL_SID != "") {
-  serviceData.apnCredentialSid = env.TWILIO_APN_CREDENTIAL_SID
-  console.log("Adding APN Credentials to service")
-} else {
-  console.log("No APN Credentials configured - add in config.js, if available.")
-}
-
 client.notify.v1.services.create(serviceData).then(function(response) {
   console.log(response);
 }).catch(function(error) {
