8640bf390d892f48d7aa820e65e2aaa773b594d4
Viktor Muller <vmuller@twilio.com>
2017-04-19 18:40:55 +0100

    Adds support for optional Endpoint parameter.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -24,7 +25,7 @@ app.get('/', function(request, response) {
 
 });
 
-function createBinding(identity, endpoint, bindingType, address, sandbox, response){
+function createBinding(opts, response){
   // Authenticate with Twilio
   var client = new twilio(env.TWILIO_ACCOUNT_SID,  env.TWILIO_AUTH_TOKEN);
 
@@ -33,16 +34,23 @@ function createBinding(identity, endpoint, bindingType, address, sandbox, respon
 
   var execCreateBinding = function(){
 
-    service.bindings.create({
-      "endpoint": endpoint,
-      "identity": identity,
-      "bindingType": bindingType,
-      "address": address
-    }).then(function(binding) {
+    var params =
+      {
+        "identity": opts.identity,
+        "bindingType": opts.BindingType,
+        "address": opts.Address
+      };
+
+    if (!_.isUndefined(opts.endpoint)) {
+      params.endpoint = opts.endpoint;
+    }
+
+    service.bindings.create(params).then(function(binding) {
       var message = 'Binding created!';
       console.log(binding);
       // Send a JSON response indicating success
       response.send({
+        endpoint: binding.endpoint,
         message: message
       });
     }).catch(function(error) {
@@ -63,16 +71,16 @@ function createBinding(identity, endpoint, bindingType, address, sandbox, respon
   // Typically this check is unnecessary and wasteful in production but very
   // useful if you are doing this the first time so we recommend removing it
   // before deploying to production.
-  if (bindingType === "apn"){
+  if (opts.BindingType === "apn"){
 
     service.fetch().then(function(serviceObj){
       return client.notify.v1.credentials(serviceObj.apnCredentialSid).fetch();
     }).then(function(credential){
-      if (credential.sandbox !== sandbox){
+      if (credential.sandbox !== opts.Sandbox){
         var message = "Mismatched APNS certificate. Make sure you are using a production certificate with a production provisioning profile and a development certificate with a development provisioning profile.";
         console.log(message);
         console.log("Credential's sandbox value: " + credential.sandbox);
-        console.log("App's sandbox value: " + sandbox);
+        console.log("App's sandbox value: " + opts.Sandbox);
         response.status(400).send({
           error: "52134",
           message: message
@@ -87,14 +95,15 @@ function createBinding(identity, endpoint, bindingType, address, sandbox, respon
         message: "Failed to create binding: " + error
       });
     });
-  } else { //if not APNS binding we can just to ahead and create the Binding
+  } else { //if not APNS binding we can just go ahead and create the Binding
     execCreateBinding();
   }
 }
 
 //Create a binding using device properties
 app.post('/register', function(request, response) {
-  createBinding(request.body.identity, request.body.endpoint, request.body.BindingType, request.body.Address, request.body.Sandbox, response);
+  createBinding(request.body,
+    response);
 });
 
 //Create a facebook-messenger binding based on the authentication webhook from Facebook
@@ -104,9 +113,13 @@ app.post('/messenger_auth', function(request, response) {
   console.log(message);
   // Set user identity using their fb messenger user id
   var identity = message.sender.id;
-  var endpoint = 'FBM@' + identity;
   //Let's create a new facebook-messenger Binding for our user
-  createBinding(identity, endpoint, 'facebook-messenger', message.sender.id, response);
+  createBinding({
+      "identity":identity,
+      "bindingType":'facebook-messenger',
+      "address":message.sender.id
+    },
+    response);
 });
 
 //Verification endpoint for Facebook needed to register a webhook.
